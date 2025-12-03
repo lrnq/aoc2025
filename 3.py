9457787f9ecf681c4d4a4f10dbbe7eef9613b3e5
lrnq <26392224+lrnq@users.noreply.github.com>
2025-12-03 22:47:25 +0100

    Update 3.py

--- 3.py
+++ 3.py
@@ -4,21 +4,20 @@ with open("3.in", "r") as f:
 lines = [list(map(int, list(x.strip()))) for x in lines]
 
 
-def f(nums, start, end, acc, target):
-    if len(acc) == target:
-        return acc
+def f(nums, start, rem):
+    if not rem:
+        return []
     mx, mx_idx = -1, -1
-    for i in range(start, len(nums) - end + 1):
+    for i in range(start, len(nums) - rem + 1):
         if nums[i] > mx:
             mx, mx_idx = nums[i], i
-    acc.append(mx)
-    return f(nums, mx_idx + 1, end - 1, acc, target)
+    return [mx] + f(nums, mx_idx + 1, rem - 1)
 
 
-ans_1 = 0
-ans_2 = 0
+ans_1, ans_2 = 0, 0
 for line in lines:
-    ans_1 += int("".join(list(map(str, f(line, 0, 2, [], 2)))))
-    ans_2 += int("".join(list(map(str, f(line, 0, 12, [], 12)))))
+    ans_1 += int("".join(list(map(str, f(line, 0, 2)))))
+    ans_2 += int("".join(list(map(str, f(line, 0, 12)))))
+
 print("Part 1:", ans_1)
 print("Part 2:", ans_2)
